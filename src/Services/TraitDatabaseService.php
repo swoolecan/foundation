7ccddacd46d5c58113dd39b4fdaac258e42f1e96
acanhost <acanstudio@canliang.wang>
2021-09-06 20:20:13 +0800

    develop foundation

--- src/Services/TraitDatabaseService.php
+++ src/Services/TraitDatabaseService.php
@@ -2,101 +2,125 @@
 
 namespace Swoolecan\Foundation\Services;
 
+use Illuminate\Database\Schema\Blueprint;
+
 trait TraitDatabaseService
 {
-    public function dumpSql($connection)
+    protected function createSql($table, $connection, $where = '', $extSetting = '')
     {
-        $infos = $this->getModelObj('dataSync')->where(['source_type' => $connection])->whereIn('status', ['9'])->get();
-        $sql = '';
-        foreach ($infos as $info) {
-            if ($info->last_id - $info->sync_id == 0 || $info->sync_id == 0) {
-                continue;
-            }
-            $sql .= '--- ' . ($info->last_id - $info->sync_id) . "\n";
-            $sql .= $this->createSql($info['code'], $connection, $info->sync_id);
-        }
-        file_put_contents("/data/log/dealdata/{$connection}.sql", $sql);//, FILE_APPEND);
-        echo $sql;
-    }
-
-    public function dumpSqlOld($connection = null)
-    {
-        //$infos = $this->getModelObj('dataSync')->where(['source_type' => $connection, 'status' => ''])->get();
-        $status = '';
-        $status = 'fixedness';
-        $status = '9';
-        $infos = $this->getModelObj('dataSync')->where(['source_type' => $connection, 'status' => $status])->get();
-        $sql = '';
-        foreach ($infos as $info) {
-            $sql .= $this->createSql($info['code'], $connection);
-        }   
-        file_put_contents("/data/log/dealdata/{$connection}{$status}.sql", $sql);//, FILE_APPEND);
-        echo $sql;exit();
-        return true;
-    }
-
-    protected function createSql($table, $connection, $start = null)
-    {
+        $dumpPath = $this->config->get('local_params.resourcePath.dumpPath');
         $config = $this->config->get('database.connections.' . $connection);
-        $sqlBase = "/usr/local/mysql/bin/mysqldump -h{$config['host']} -u{$config['username']} -P{$config['port']} -p'{$config['password']}' --default-character-set=utf8mb4 --set-gtid-purged=off {$config['database']} {{TABLE}} {{WHERE}} > /data/wwwroot/happy-writing/public/docs/d/sql1/{{TABLE}}.sql";
-        //$where = empty($start) ? '' : '--where="id >= ' . $start .' "';
-        $where = '--where="id >= 5000001 AND id < 10000001';
+        $sqlBase = "/usr/local/mysql/bin/mysqldump -h{$config['host']} -u{$config['username']} -P{$config['port']} -p'{$config['password']}' --default-character-set=utf8mb4 {{EXTSETTING}} --set-gtid-purged=off {$config['database']} {{TABLE}} {{WHERE}} > {$dumpPath}/{{TABLE}}.sql";
+        //$where = '--where="id >= 5000001 AND id < 10000001';
+        //$extSetting = ' --lock-tables=false'
 
-        $sql = str_replace(['{{TABLE}}', '{{WHERE}}'], [$table, $where], $sqlBase) . "\n";
+        $sql = str_replace(['{{TABLE}}', '{{WHERE}}', '{{EXTSETTING}}'], [$table, $where, $extSetting], $sqlBase) . "\n";
         return $sql;
     }
 
-    public function recordDataSync($schema, $connection, $tables = null)
+    public function getTableDatas($schema, $connection, $withCurrentIncrement = false)
     {
-        $tables = is_null($tables) ? $this->getTableDatas($schema, $connection) : $tables;
-        //print_R($tables);exit();
-        $model = $this->getModelObj('bigdata-dataSync');
-        $sql = 'INSERT INTO `wp_data_sync`(`code`, `source_type`, `name`, `last_num`, `last_id`, `sync_id`, `created_at`, `updated_at`) VALUES';
-        $updateSqlBase = "UPDATE `wp_data_sync` SET {{SET}} WHERE `code` = '{{TABLE}}' AND `source_type` = '{$schema}';\n";
-        $updateSql = '';
-        foreach ($tables as $table => $data) {
-            $where = ['code' => $table];
-            $exist = $model->where($where)->first();
-            $lastId = intval($data['last_id']);
-            if ($exist) {
-                $syncId = $data['syncId'] ?? 0;
-                $setStr = "`sync_id` = {$syncId}";
-                //$setStr = "`last_id` = {$lastId}, `sync_id` = {$syncId}";
-                $updateSql .= str_replace(['{{SET}}', '{{TABLE}}'], [$setStr, $table], $updateSqlBase);
-            } else {
-                $updatedAt = empty($data['updated_at']) ? date('Y-m-d H:i:s') : $data['updated_at'];
-                $syncId = $data['syncId'] ?? 0;
-                $sql .= "('{$data['code']}', '{$schema}', '{$data['name']}', '{$data['last_num']}', '{$lastId}', '{$syncId}', '{$data['created_at']}', '{$updatedAt}'),\n";
-            }
-        }
-        echo trim(trim($sql), ',') . ';';
-        echo $updateSql;
-        exit();
-    }
-
-    public function getTableDatas($schema, $connection = null)
-    {
-        $db = $connection ? \DB::connection($connection) : \DB::connection();
-        $results = $db->table(\DB::raw('information_schema.TABLES'))->where('TABLE_SCHEMA', $schema)->get();
+        $results = $this->getConnection($connection)->table(\DB::raw('information_schema.TABLES'))->where('TABLE_SCHEMA', $schema)->get();
         $fields = [
-            'code' => 'TABLE_NAME',
-            'name' => 'TABLE_COMMENT',
-            'last_num' => 'TABLE_ROWS',
-            'last_id' => 'AUTO_INCREMENT',
+            'name' => 'TABLE_NAME',
+            'comment' => 'TABLE_COMMENT',
+            'table_row' => 'TABLE_ROWS',
+            'increment' => 'AUTO_INCREMENT',
             'created_at' => 'CREATE_TIME',
             'updated_at' => 'UPDATE_TIME',
         ];
         $datas = [];
         foreach ($results as $result) {
-            $code = $result->TABLE_NAME;
+            $name = $result->TABLE_NAME;
             foreach ($fields as $field => $attr) {
-                $datas[$code][$field] = $result->$attr;
+                $value = $result->$attr;
+                $value = in_array($field, ['row', 'increment']) ? intval($value) : $value;
+                $datas[$name][$field] = $value;
             }
-            if (!in_array($code, ['tmp_order', 'tmp_order_id'])) {
-                $info = $db->SELECT("SELECT * FROM `{$schema}`.`{$code}` ORDER BY `id` DESC LIMIT 1;");
-                $datas[$code]['syncId'] = empty($info) ? 0 : $info[0]->id;
+            if ($withCurrentIncrement) {
+                $datas[$name]['currentIncrement'] = $this->currentIncrement($connection, $schema, $name);
             }
         }
         return $datas;
+    }
+
+    public function getColumnDatas($connection, $schema, $table = null, $where = null)
+    {
+        $where = is_null($where) ? [] : $where;
+        $where['TABLE_SCHEMA'] = $schema;
+        if (!is_null($table)) {
+            $where['TABLE_NAME'] = $table;
+        }
+        //$results = $this->getConnection($connection)->table(\DB::raw('information_schema.COLUMNS'))->where($where)->whereIn('COLUMN_NAME', ['created_at', 'updated_at'])->get();
+        $results = $this->getConnection($connection)->table(\DB::raw('information_schema.COLUMNS'))->where($where)->where('COLUMN_NAME', 'like', '%_at')->get();
+        //$results = $this->getConnection($connection)->table(\DB::raw('information_schema.COLUMNS'))->where($where)->get();
+        $fields = [
+            'name' => 'COLUMN_NAME',
+            'comment' => 'COLUMN_COMMENT',
+            'table' => 'TABLE_NAME',
+            'database' => 'TABLE_SCHEMA',
+            'type' => 'DATA_TYPE',
+            'default' => 'COLUMN_DEFAULT',
+            'extra' => 'EXTRA',
+        ];
+        $datas = [];
+        foreach ($results as $result) {
+            $name = $result->COLUMN_NAME;
+            foreach ($fields as $field => $attr) {
+                if (is_null($table)) {
+                    $datas[$result->TABLE_NAME][$name][$field] = $result->$attr;
+                } else {
+                    $datas[$name][$field] = $result->$attr;
+                }
+            }
+        }
+        return $datas;
+    }
+
+    public function currentIncrement($connection, $schema, $table, $onlyField = false)
+    {
+        $column = $this->getColumnDatas($connection, $schema, $table, ['EXTRA' => 'auto_increment']);
+        if (empty($column)) {
+            //echo 'noauto-----' . $table . "\n";
+            return -1;
+        }
+        $column = array_values($column);
+        $field = $column[0]['name'];
+        if ($field != 'id') {
+            //echo 'ffffffffff--' .$table . '==' . $field . "\n";
+            $schemaTmp = str_replace('bak_', '', $schema);
+            //echo "UPDATE `wp_data_sync` SET `auto_field` = '{$field}' WHERE `source_type` = '{$schemaTmp}' AND `code` = '{$table}';\n";
+        }
+        $info = $this->getConnection($connection)->SELECT("SELECT * FROM `{$schema}`.`{$table}` ORDER BY `{$field}` DESC LIMIT 1;");
+        return empty($info) ? 0 : $info[0]->$field;
+    }
+
+    public function getIndexDatas($connection, $tableName, $pointIndex = 'primary')
+    {
+        $db = \Schema::connection($connection);
+        $indexes = [];
+        $db->table($tableName, function (Blueprint $table) use ($tableName, $db, & $indexes, $pointIndex) {
+            $sm = $db->getConnection()->getDoctrineSchemaManager();
+            $allIndexes = $sm->listTableIndexes($tableName);
+            if (empty($pointIndex)) {
+                $indexes = $allIndexex;
+                return ;
+            }
+            $indexes = array_key_exists($pointIndex, $allIndexes) ? $allIndexes[$pointIndex] : false;
+
+            return $indexes;
+            /*if (array_key_exists('index_name', $indexesFound)) {
+                $table->dropUnique("index_name");
+            }*/
+        });
+        //$index = $this->getIndexDatas($db, $table);
+        //$indexColumns = $index->getColumns();
+        //print_R($indexColumns);exit();
+        return $indexes;
+    }
+
+    protected function getConnection($connection)
+    {
+        return \DB::connection($connection);
     }
 }
